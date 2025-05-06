ffd997652259ad33d89c67011f76e67f9148c032
FancyBread <60786971+alonchen410@users.noreply.github.com>
2025-05-06 11:38:13 +0300

    Add files via upload
    very basic form of the server and client of the game with a cool receiver on the server

---
+++ client.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+import time
+
+global client_socket
+client_socket = None
+
+def init():
+    global client_socket
+    client_socket = socket.socket()
+    client_socket.settimeout(60)
+
+def ConnectToServer(RHOST, RPORT):
+    global client_socket
+
+    while True:
+        try:
+            client_socket.connect((RHOST, RPORT))
+            break
+        except:
+            print("Connection failed, retrying in 1 seconds...")
+            time.sleep(1)
+
+def main():
+    global client_socket
+
+    #host = input("Enter the server's ip address:")
+    #port = int(input("Enter the server's port:"))
+    host = "localhost"
+    port = 4040
+
+    init()
+    ConnectToServer(host, port)
+
+    time.sleep(1)
+    client_socket.send("[MESSAGE]hello[MESSAGE]".encode())
+    client_socket.send("[MESSAGE]hello[MES]".encode())
+    client_socket.send("hello[MESSAGE]".encode())
+
+main()
