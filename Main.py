ecd67741b3232247504021ca25b21660904bfb35
KBjona <Jona10112010@gmail.com>
2025-05-07 14:47:53 +0300

    changed the drawtile to x,y made a setup function, created a defender class and made everything that needed to be global global.

--- Main.py
+++ Main.py
@@ -1,20 +1,26 @@
 import pygame
 import time
+import network.client as client
+
+global screenColor, Width, Height, Time, tilewidth, tileheight
+global screen, clock, tiles, bg
 
 Width = 800
 Height = 600
 Time = 60
 tilewidth = 8
-tileheight =5
+tileheight = 5
 screenColor = (26, 138, 35)
 
-screen = pygame.display.set_mode((Width, Height))
-pygame.display.set_caption("presidents vs nazis")
-clock = pygame.time.Clock()
-tiles = [[0 for _ in range(tilewidth)] for _ in range(tileheight)]
-
-pygame.init()
-
+def setup():
+    global screen, clock, tiles, bg
+    pygame.init()
+    screen = pygame.display.set_mode((Width, Height))
+    pygame.display.set_caption("presidents vs nazis")
+    clock = pygame.time.Clock()
+    tiles = [[0 for _ in range(tilewidth)] for _ in range(tileheight)]
+    bg = pygame.image.load("pvzTypeGame/images/peashooter.png").convert_alpha()
+    bg = pygame.transform.scale(bg, (70, 70))
 
 def update():
     pygame.display.update()
@@ -22,45 +28,53 @@ def update():
     time.sleep(0.05)
 
 
+class Defender:
+    def __init__(self, type, x, y, damage, health, image):
+        self.type = type
+        self.x = x
+        self.y = y
+        self.damage = damage
+        self.health = health
+        self.image = image
+    def damaged(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            pass # should be death 
+
+
+
+
+
+
 def draw_grid():
-    for i in range(tilewidth):
-        for j in range(tileheight):
-            #print(tiles)
-                sqr = pygame.rect.Rect(i * 100, j * 100 + 60, 80, 80)
-                if tiles[j][i] == 0:
-                    pygame.draw.rect(screen, (255, 255, 255), sqr)
-
-                elif tiles[j][i] == 1:
-                    pygame.draw.rect(screen, (0, 0, 0), sqr)
-                
-
-                if pygame.mouse.get_pressed()[0]:
-                    if sqr.collidepoint(pygame.mouse.get_pos()):
-                        tiles[j][i] = 1
-                        print(j, i)
-                        #print("you clicked on the square")
+    for x in range(tilewidth):
+        for y in range(tileheight):
+            sqr = pygame.Rect(x * 95, y * 95 + 70, 70, 70)
+            if tiles[y][x] == 0:
+                pygame.draw.rect(screen, (36, 200, 100), sqr)
+            elif tiles[y][x] == 1:
+                screen.blit(bg, (x * 95, y * 95 + 70))
+            if pygame.mouse.get_pressed()[0]:
+                if sqr.collidepoint(pygame.mouse.get_pos()):
+                    tiles[y][x] = 1
+                    print(y, x)
 
 def main():
-
+    setup()
     running = True
-    
     while running:
-
         screen.fill(screenColor)
-    
         draw_grid()
-    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-    
-                print("you clicked the X button")
+                print("You clicked the X button")
         update()
-
     pygame.quit()
+
 try:
     main()
 except SystemExit:
-    print("you closed the window")
+    print("You closed the window")
 except Exception as e:
-    print("An error occurred:", )
+    print("An error occurred:", e)
