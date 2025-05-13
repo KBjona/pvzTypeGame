2d89d4379e70b972ae8655dba6f20cc3dbd3580c
KBjona <Jona10112010@gmail.com>
2025-05-13 19:06:24 +0300

    created a getinput func cus cool and alon asked like a good boy

--- Main.py
+++ Main.py
@@ -86,9 +86,9 @@ def attackerSetup():
 
     tiles = [[0 for _ in range(tilewidth)] for _ in range(tileheight)]
 
-    bg = pygame.image.load("images/yuvalsbg.png").convert_alpha()
-    bg = pygame.transform.scale(bg, (Width, Height))
-
+    bg = pygame.image.load("images/updatebg.png").convert_alpha()
+    bg = pygame.transform.scale(bg, (Width + 200, Height))
+    
     setup_defenders()
 
     projectiles = []
@@ -162,7 +162,7 @@ def sunflower_action():
                             defenderMoney += 50
 
 def draw_grid(status):
-    global defenderMoney, current_defender
+    global defenderMoney, current_defender,x ,y
     for x in range(tilewidth):
         for y in range(tileheight):
             sqr = pygame.Rect(x * 80 + 60 , y * 80 + 100, 60, 60)
@@ -172,17 +172,25 @@ def draw_grid(status):
                 defender = tiles[y][x]
                 screen.blit(defender.image, (x * 80 + 53, y * 80 + 92))
     
-            if pygame.mouse.get_pressed()[0] and sqr.collidepoint(pygame.mouse.get_pos()) and status == "defender": 
-                if current_defender == 1 and defenderMoney >= 50 and tiles[y][x] == 0:
-                    defenderMoney -= 50
-                    tiles[y][x] = Defender("sunflower", x - 30, y - 15, 10, 100, 100, sunflower)
-                    print(y, x)
-                    print("placed sunflower and -50 defenderMoney")
-                elif current_defender == 2 and defenderMoney >= 100 and tiles[y][x] == 0:
-                    defenderMoney -= 100
-                    tiles[y][x] = Defender("peashooter", x - 30, y - 15, 10, 100, 100, peashooter)
-                    print(y, x)
-                    print("placed peashooter and -100 defenderMoney")
+            if pygame.mouse.get_pressed()[0] and sqr.collidepoint(pygame.mouse.get_pos()): 
+                getinput(status)
+
+def getinput(status):
+    global defenderMoney, current_defender, x, y
+
+    if status == "defender":
+        if current_defender == 1 and defenderMoney >= 50 and tiles[y][x] == 0:
+            defenderMoney -= 50
+            tiles[y][x] = Defender("sunflower", x - 30, y - 15, 10, 100, 100, sunflower)
+            print(y, x)
+            print("placed sunflower and -50 defenderMoney")
+        elif current_defender == 2 and defenderMoney >= 100 and tiles[y][x] == 0:
+            defenderMoney -= 100
+            tiles[y][x] = Defender("peashooter", x - 30, y - 15, 10, 100, 100, peashooter)
+            print(y, x)
+            print("placed peashooter and -100 defenderMoney")
+
+
 def place_attacker():
     idk = pygame.image.load("images/mize.png").convert_alpha()
     idk = pygame.transform.scale(idk, (70, 70))
@@ -190,15 +198,19 @@ def place_attacker():
         screen.blit(idk, ( 850, y * 80 + 92))
 
 
-def display_current_defender():
+def display_current(status):
     global current_defender
-    draw_text("Current Defender is:", pygame.font.SysFont("Arial", 30), (0, 255, 0), 425, 40)
-    if current_defender == 1:
-        screen.blit(sunflower, (700, 20))
-    elif current_defender == 2:
-        screen.blit(peashooter, (700, 20))
-    else:
-        pass
+    if status == "defender":
+        draw_text("Current Defender is:", pygame.font.SysFont("Arial", 30), (0, 255, 0), 425, 40)
+        if current_defender == 1:
+            screen.blit(sunflower, (700, 20))
+        elif current_defender == 2:
+            screen.blit(peashooter, (700, 20))
+        else:
+            pass
+    elif status == "attacker":
+        draw_text("Current Attacker is:", pygame.font.SysFont("Arial", 30), (0, 255, 0), 425, 40)
+
 
 def defenderGameLoop():
     setup()
@@ -210,7 +222,7 @@ def defenderGameLoop():
         draw_grid("defender")
         draw_projectiles()
         draw_text("Money: " + str(defenderMoney), font, (255,255,255), 0,0)
-        display_current_defender()
+        display_current("defender")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -229,7 +241,7 @@ def attackerGameLoop():
         place_attacker()
         draw_projectiles()
         draw_text("Money: " + str(attackerMoney), font, (255,255,255), 0,0)
-        display_current_defender()
+        display_current("attacker")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -240,7 +252,7 @@ def attackerGameLoop():
 
 
 def main():
-    #defenderGameLoop()
+    defenderGameLoop()
     attackerGameLoop()
 
 
